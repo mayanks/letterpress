cc4cbaf06fe066714f77c05bee9d9ba32b3a23bc
Mayank Sharma <mayanks@gmail.com>
2012-11-23 01:40:18 +0530

    logging in user on facebook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+require 'base64'
+require 'json'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
@@ -5,4 +8,23 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, :alert => exception.message
   end
 
+  before_filter :check_facebook_login
+
+  def check_facebook_login
+    if params[:signed_request]
+      (encoded_sig,encoded_payload) = params[:signed_request].split(/\./)
+      data = JSON.parse(base64_url_decode(encoded_payload))
+      if data["user_id"]
+        u = User.find_by_uid(data["user_id"])
+        if u 
+          sign_in(u, :event => :authentication)
+        end
+      end
+    end
+  end
+
+  def base64_url_decode(str)
+    str += '=' * (4 - str.length.modulo(4))
+    Base64.decode64(str.gsub("-", "+").gsub("_", "/"))
+  end
 end
