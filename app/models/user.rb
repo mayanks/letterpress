7671fdf449ee4e489fe79fd426d3cf97fa453ba0
Mayank Sharma <mayanks@gmail.com>
2012-11-22 22:57:29 +0530

    removed floating layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ActiveRecord::Base
   
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    #puts "Token : #{auth.credentials.token}"
     unless user
       user = User.create(name:auth.extra.raw_info.name,
                          provider:auth.provider,
