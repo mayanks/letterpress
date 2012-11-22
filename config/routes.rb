2bd8e9b2be3f9627bf3df5c6f3f8fddd191dc821
Mayank Sharma <mayanks@gmail.com>
2012-11-23 00:48:25 +0530

    added privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ LetterPress::Application.routes.draw do
   get "/letterpress", :to => "games#letterpress"
   get "/howtoplay", :to => "games#howtoplay"
   get "/contact", :to => "games#contact"
+  get "/privacy", :to => "games#privacy"
+  get "/tos", :to => "games#tos"
   root :to => "games#index"
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_scope :user do
