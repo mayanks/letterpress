7518d9b9953f41f4e3b555aa395e65baf5c8128d
Mayank Sharma <mayanks@gmail.com>
2012-11-23 23:38:40 +0530

    added letterpress dictionary

--- config/initializers/load_words.rb
+++ config/initializers/load_words.rb
@@ -1,2 +1,3 @@
 #Game.setup(Rails.root.join('words.txt'))
-Game.setup(Rails.root.join('words.2'))
+#Game.setup(Rails.root.join('words.2'))
+Game.setup(Rails.root.join('letterpress_alphabetical.txt'))
