25c484c73e712e297314ff9a649d77b976f86c1c
Mayank Sharma <mayanks@gmail.com>
2013-02-02 15:26:15 +0530

    some style changes for mobile view

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ LetterPress::Application.routes.draw do
   get "/tos", :to => "games#tos"
   get "/leaderboard", :to => "home#leaderboard"
   get "/stats", :to => "home#stats"
+  get "/home", :to => "home#index"
   root :to => "games#index"
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_scope :user do
